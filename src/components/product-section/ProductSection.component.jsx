3e587094e42b95f89b18d3364cc90e3ebfa57c13
tristanbeykusuma <tristanbeykusuma@gmail.com>
2023-10-23 06:20:20 +0800

    test workflow

--- src/components/product-section/ProductSection.component.jsx
+++ src/components/product-section/ProductSection.component.jsx
@@ -4,7 +4,7 @@ import ProductLoop from '../product-loop/ProductLoop.component'
 const ProductSection = () => {
   return (
     <div className='m-5'>
-      <h3>BEST SELLING PRODUCTS</h3>
+      <h3>Best Selling Products</h3>
       <hr/>
       <div className='row'>
       <ProductLoop/>
