d536324ad14b886887b81f6ed5f0ad3adf5664b6
tristanbeykusuma <tristanbeykusuma@gmail.com>
2023-10-23 06:08:29 +0800

    deploy

--- src/layouts/about/About.layout.jsx
+++ src/layouts/about/About.layout.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import { useState, useEffect } from "react";
 import AboutSection from "../../components/about-section/AboutSection.component";
 import YoutubeSection from "../../components/youtube-section/YoutubeSection.component";
 import BlogSection from "../../components/blog-section/BlogSection.component";
 
 const About = () => {
+  const [cartItems,setCartItems] = useState({ context: 'random', toolId: 'facts/plants/' });
+
   return (
     <>
       <Outlet />
